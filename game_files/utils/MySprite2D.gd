83558a96687f53a2413f675a604928e8c88e8773
jlemon21 <jlemon21@gmail.com>
2023-03-30 12:10:16 -0400

    Update MySprite2D.gd
    - Removed Timer node from MySprite2D, now handling sprite flicker logic in code.

--- game_files/utils/MySprite2D.gd
+++ game_files/utils/MySprite2D.gd
@@ -1,17 +1,34 @@
 extends Sprite2D
 class_name MySprite2D
 
-@onready var _FlickerTimer: Timer = $FlickerTimer
+var _flicker: bool = false
+var _target_flicker_time: float = 0.0 			# amount of time sprite flicker will be active
+var _total_flicker_time: float = 0.0 			# amount of time the sprite flicker has been happening
+var _flicker_interval: float = 0.0				# amount of time between changes in visibility 
+var _current_flicker_interval: float = 0.0		# amount of time since the last change in visibility
 
+func _physics_process(delta: float) -> void:
+	if _flicker:
+		_total_flicker_time += delta
+		if _total_flicker_time >= _target_flicker_time:
+			stop_sprite_flicker()
+		else:
+			_current_flicker_interval += delta
+			if _current_flicker_interval >= _flicker_interval:
+				visible = !visible
+				_current_flicker_interval = 0.0
 
-func start_sprite_flicker(flicker_interval: float = 0.05) -> void:
-	_FlickerTimer.start(flicker_interval)
+func start_sprite_flicker(target_time: float = 1.0, flicker_interval: float = 0.05) -> void:
+	_flicker = true
+	_target_flicker_time = target_time
+	_flicker_interval = flicker_interval
+	_total_flicker_time = 0.0
+	_current_flicker_interval = 0.0
 
 func stop_sprite_flicker() -> void:
-	_FlickerTimer.stop()
 	visible = true
-
-func _on_flicker_timer_timeout():
-	visible = !visible
-	_FlickerTimer.start()
-
+	_flicker = false
+	_target_flicker_time = 0.0
+	_flicker_interval = 0.0
+	_total_flicker_time = 0.0
+	_current_flicker_interval = 0.0

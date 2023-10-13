6ceaa232d03ac81da8166d42299fb78b695d20c2
jlemon21 <jlemon21@gmail.com>
2023-10-13 17:07:40 -0400

    Update FiniteStackMachine.gd
    - Fixed regression in FiniteStackMachine that got rid of the initialize() function at some point in the distant past.
    - Updated some naming conventions and handling to better reflect FiniteStackMachineState instances being Nodes now instead of Resources.

--- game_files/components/finite_stack_machine/FiniteStackMachine.gd
+++ game_files/components/finite_stack_machine/FiniteStackMachine.gd
@@ -5,7 +5,7 @@ class_name FiniteStackMachine
 var state_parent: Node
 # Node that the stateStack is attached to
 
-var stack_machine = []
+var state_stack: Array[FiniteStackMachineState] = []
 # This member array is the heart of the whole operation.
 # It looks like this:
 # [[state_object1, [arg1, arg2, ...]], [state_object2, [arg1, arg2, ...]], ...]
@@ -17,13 +17,17 @@ var default_state: Array
 #signal state_time_changed(new_value)
 signal stack_machine_changed
 
+func initialize(parent_scene: Node, default_state_path: String, default_state_arguments: Array = []) -> void:
+	state_parent = parent_scene
+	default_state = [default_state_path, default_state_arguments]
+
 func process(delta: float) -> void:
 	# Method for processing the current state @ pos 0
 	#if stack_machine.size() == 0:
 	#	print("STACK MACHINE EMPTY FOR ", get_parent().name)
-	if stack_machine.size() > 0:
+	if state_stack.size() > 0:
 		# get top state info
-		var state_object = stack_machine[0]
+		var state_object: FiniteStackMachineState = state_stack[0]
 		
 		# Check for first time running, and start state
 		if state_object.started == false:
@@ -44,9 +48,9 @@ func process(delta: float) -> void:
 		process(delta) # and try to run it right away
 
 func physics_process(delta: float) -> void:
-	if stack_machine.size() > 0:
+	if state_stack.size() > 0:
 		# get top state info
-		var state_object = stack_machine[0]
+		var state_object: FiniteStackMachineState = state_stack[0]
 		
 		# Check for first time running, and start state
 		if state_object.started == false:
@@ -68,9 +72,9 @@ func physics_process(delta: float) -> void:
 
 func input(event: InputEvent):
 	# Method for processing the current state @ pos 0
-	if stack_machine.size() > 0:
+	if state_stack.size() > 0:
 		# get top state info
-		var state_object = stack_machine[0]
+		var state_object: FiniteStackMachineState = state_stack[0]
 		
 		# Check for first time running, and start state
 		if state_object.started == false:
@@ -89,13 +93,13 @@ func input(event: InputEvent):
 
 func push(state):
 	# add a state to front of state stack
-	stack_machine.push_front(state)
-	emit_signal("stack_machine_changed")
+	state_stack.push_front(state)
+	stack_machine_changed.emit()
 
 func add(state):
 	# add a state to end of state stack
-	stack_machine.append(state)
-	emit_signal("stack_machine_changed")
+	state_stack.append(state)
+	stack_machine_changed.emit()
 
 func push_create(state_path, args = []):
 	# create and add a state class with args to front of state stack
@@ -107,58 +111,60 @@ func add_create(state_class, args = []):
 
 func pop():
 	# pop the top state from the from state stack
-	var state_object = stack_machine.pop_front()
+	var old_state: FiniteStackMachineState = state_stack.pop_front()
 	# process on_end event for the popped state
-	state_object.on_end()
-	emit_signal("stack_machine_changed")
+	old_state.on_end()
+	old_state.queue_free()
+	stack_machine_changed.emit()
 
 func state_create(state_path, args = []):
-	# creates an entry appropriate for the stack_machine array
+	# creates an entry appropriate for the state_stack array
 	# [state_ref, [arg1, arg2, ...]]
 	#var state_object = state_class.new() 
-	var state_object = load(state_path).new()
+	var state_object: FiniteStackMachineState = load(state_path).new()
 	state_object.state_parent = state_parent
 	#state_object.state_class = state_class # ? 
 	state_object.args = args
+	add_child(state_object)
 	return state_object
 
 func get_current_state():
-	return stack_machine[0] 
+	return state_stack[0] 
 
 	### MY SPECIAL NEW FUNCTIONS ###
-func pop_until(target_state_name: String) -> void:
+func pop_until(target_state_name: String) -> void: # this may be dangerous now that states are nodes? lmao
 	if _state_exists_in_stack(target_state_name):
-		while (get_current_state().name != target_state_name):
+		while (get_current_state().state_name != target_state_name):
 			pop()
 
 func _state_exists_in_stack(target_state_name: String) -> bool:
-	for state in stack_machine:
-		if state.name == target_state_name:
+	for state in state_stack:
+		if state.state_name == target_state_name:
 			return true
 	return false
 
-func get_state_below_self() -> Resource:
-	if stack_machine.size() == 1:
+func get_state_below_self() -> FiniteStackMachineState:
+	if state_stack.size() == 1:
 		return null
-	return stack_machine[1]
+	return state_stack[1]
 
-func get_state_below(state_name: String) -> Resource:
+func get_state_below(state_name: String) -> FiniteStackMachineState:
 	var state_index: int = _get_index_of_state(state_name)
-	if state_index == -1 or state_index >= stack_machine.size() - 1:
+	if state_index == -1 or state_index >= state_stack.size() - 1:
 		return null
-	return stack_machine[state_index + 1]
+	return state_stack[state_index + 1]
 
 func _get_index_of_state(target_name: String) -> int:
 	var index: int = -1
-	for i in range(stack_machine.size()):
-		if stack_machine[i].name == target_name:
+	for i in range(state_stack.size()):
+		if state_stack[i].state_name == target_name:
 			index = i
 	return index
 
 func get_current_state_name() -> String:
-	if stack_machine.size() == 0:
+	if state_stack.size() == 0:
 		return "NONE"
-	return stack_machine[0].name
+	return state_stack[0].state_name
 
 
 
